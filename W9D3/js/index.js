4a680d30ce31ccc60ed86e3ffceff28018d554f1
duskstone <jedgardromero@gmail.com>
2020-05-13 23:48:13 -0400

    fixed function typo, added console log after AJAX request

--- W9D3/js/index.js
+++ W9D3/js/index.js
@@ -1,7 +1,8 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
-$ajax({
+// do not forget your "." operator or $ajax is not defined
+$.ajax({
     type: 'GET',
     url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b",
     success(data) {
@@ -14,3 +15,4 @@ $ajax({
 });
 
 // Add another console log here, outside your AJAX request
+ console.log("code is After AJAX Request but logs before AJAX request");
